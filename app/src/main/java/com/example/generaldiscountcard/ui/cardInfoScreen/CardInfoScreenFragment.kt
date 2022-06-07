d5fa8f6ea09477b489ce15251d863cb45cc574b0
Danil <emurashin@gmail.com>
2022-06-08 02:43:05 +0500

    add: fun delete card

--- app/src/main/java/com/example/generaldiscountcard/ui/cardInfoScreen/CardInfoScreenFragment.kt
+++ app/src/main/java/com/example/generaldiscountcard/ui/cardInfoScreen/CardInfoScreenFragment.kt
@@ -4,16 +4,24 @@ import android.graphics.Bitmap
 import android.graphics.Color.BLACK
 import android.graphics.Color.WHITE
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.generaldiscountcard.R
+import com.example.generaldiscountcard.dataClasses.Card
 import com.example.generaldiscountcard.dataClasses.CardInfoObject
+import com.example.generaldiscountcard.dataClasses.User
+import com.example.generaldiscountcard.dataClasses.cardArrayList
 import com.example.generaldiscountcard.databinding.FragmentCardInfoScreenBinding
-
+import com.example.generaldiscountcard.utilits.CARD_LIST
+import com.example.generaldiscountcard.utilits.NODE_USERS
+import com.example.generaldiscountcard.utilits.REF_DATABASE_ROOT
+import kotlinx.android.synthetic.main.fragment_card_info_screen.*
 
 
 class CardInfoScreenFragment : Fragment() {
@@ -31,7 +39,8 @@ class CardInfoScreenFragment : Fragment() {
         _binding = FragmentCardInfoScreenBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textViewCardShopName: TextView = root.findViewById<View>(R.id.card_shop_name) as TextView
+        val textViewCardShopName: TextView =
+            root.findViewById<View>(R.id.card_shop_name) as TextView
         val textViewCardNumber: TextView = root.findViewById<View>(R.id.card_number) as TextView
         textViewCardShopName.text = CardInfoObject.cardInfoShopName
         textViewCardNumber.text = CardInfoObject.cardNumber.toString()
@@ -42,10 +51,48 @@ class CardInfoScreenFragment : Fragment() {
             bottomFragment.show(parentFragmentManager, "TAG")
         }
 
+        val deleteButton : Button = binding.buttonDelete
+        deleteButton.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_navigation_card_info_to_navigation_wallet)
+            deleteCard(User.UserName, Card(
+                CardInfoObject.cardInfoShopName,
+                CardInfoObject.cardNumber,
+                CardInfoObject.imageIdFrontSide,
+                CardInfoObject.imageIdBackSide))
+        }
+
+//        topAppBar.setNavigationOnClickListener {
+//            deleteCard(User.UserName, Card(
+//                CardInfoObject.cardInfoShopName,
+//                CardInfoObject.cardNumber,
+//                CardInfoObject.imageIdFrontSide,
+//                CardInfoObject.imageIdBackSide))
+//        }
+
+//        topAppBar.setOnMenuItemClickListener { menuItem ->
+//            when (menuItem.itemId) {
+//
+//                R.id.more -> {
+//                    Log.d("Wallet", "Карта удалена")
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+
 
         return root
     }
 
+    fun deleteCard(userName: String, card: Card) {
+        REF_DATABASE_ROOT.child(NODE_USERS).child(userName).child(CARD_LIST).child(card.cardName)
+            .removeValue()
+        cardArrayList.remove(Card(
+            card.cardName,
+            card.cardNumber,
+            card.imageIdFrontSide,
+            card.imageIdBackSide))
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/generaldiscountcard/ui/walletScreen/WalletScreenFragment.kt
+++ app/src/main/java/com/example/generaldiscountcard/ui/walletScreen/WalletScreenFragment.kt
@@ -1,11 +1,10 @@
 package com.example.generaldiscountcard.ui.walletScreen
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.GridView
 import androidx.fragment.app.Fragment
@@ -16,15 +15,12 @@ import com.example.generaldiscountcard.adapters.GridViewAdapter
 import com.example.generaldiscountcard.dataClasses.CardInfoObject
 import com.example.generaldiscountcard.dataClasses.cardArrayList
 import com.example.generaldiscountcard.databinding.FragmentWalletScreenBinding
-import com.example.generaldiscountcard.utilits.*
-import kotlinx.android.synthetic.main.fragment_card_info_screen.*
 
 
 class WalletScreenFragment : Fragment() {
 
     private var _binding: FragmentWalletScreenBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,8 +31,7 @@ class WalletScreenFragment : Fragment() {
         val root: View = binding.root
 //        val view: View = inflater.inflate(R.layout.fragment_wallet_screen, container, false)
 
-        if (cardArrayList.isEmpty())
-        {
+        if (cardArrayList.isEmpty()) {
             val ft: FragmentTransaction = this.requireFragmentManager().beginTransaction()
             ft.detach(this)
             ft.attach(this)
@@ -54,40 +49,50 @@ class WalletScreenFragment : Fragment() {
             OnItemClickListener { parent, view, position, id ->
                 val selectedItem = parent.getItemAtPosition(position).toString()
 
-
                 val a = splitGridViewItem(selectedItem)
                 CardInfoObject.cardInfoShopName = a[0]
                 CardInfoObject.cardNumber = a[1].toBigInteger()
                 CardInfoObject.imageIdFrontSide = a[2].toInt()
                 CardInfoObject.imageIdBackSide = a[3].toInt()
-                view.findNavController().navigate(R.id.action_navigation_wallet_to_navigation_card_info)
+                view.findNavController()
+                    .navigate(R.id.action_navigation_wallet_to_navigation_card_info)
             }
-
-
-//        topAppBar.setNavigationOnClickListener {
-//            // Handle navigation icon press
-//        }
-//
-//        topAppBar.setOnMenuItemClickListener { menuItem ->
-//            when (menuItem.itemId) {
-//
-//                R.id.more -> {
-//                    Log.d("Wallet", "Карта удалена")
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
 
         return root
     }
+
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setHasOptionsMenu(true)
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.top_app_bar_card_info, menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            R.id.more -> {
+//                deleteCard(User.UserName, Card(
+//                    CardInfoObject.cardInfoShopName,
+//                    CardInfoObject.cardNumber,
+//                    CardInfoObject.imageIdFrontSide,
+//                    CardInfoObject.imageIdBackSide))
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    fun splitGridViewItem(item : String): ArrayList<String> {
+    fun splitGridViewItem(item: String): ArrayList<String> {
         val cardList = arrayListOf<String>()
         for (e in item.split('(', ')')[1].split(','))
             cardList.add(e.split('=')[1])
